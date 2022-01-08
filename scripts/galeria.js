55a671824e5a050128ce38a9c265162aceff63b0
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-01-08 20:05:39 +0100

    galeria resuelta pero falta resolver el scroll

---
+++ scripts/galeria.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", function () {//todo lo que debe esperar a que se cargue la pagina se mete en esta función
+    //Entorno:
+    //document.getElementById('originalSize').style.backgroundImage = 'url("./img/")';
+    let miniaturas = document.querySelectorAll('#preview img');//todas las miniaturas
+    for(let i=0;i<miniaturas.length;i++){
+        let img=miniaturas[i];
+        img.addEventListener('mouseover', function(event){
+            console.log(event.target.src);//objeto que escuchó el evento
+            let rutaImagen = event.target.src;
+            //document.getElementById('originalSize').style.backgroundImage = 'url("'+rutaImagen+'")';//se construye el url
+            document.getElementById('originalSize').innerHTML='<img src="'+rutaImagen+'">';
+        });
+        
+    }
+    //aqui muestro al inicio siempre la primera imagen de todas, al no iterar solo coge la primera  
+    let ruta = document.querySelector('#preview img').src;
+    
+    document.getElementById('originalSize').innerHTML='<img src="'+ruta+'">';
+    
+    
+    //alert('hola');
+
+
+});
